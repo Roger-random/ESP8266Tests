772c99bcd7fd161f35620cc743575964de32b098
Roger Cheng <roger.random@outlook.com>
2020-12-02 20:11:51 -0800

    Template for application secrets

---
+++ VoltageLogger/include/secrets.h
@@ -0,0 +1,14 @@
+// Application specific secrets that should not be committed into GitHub.
+
+// WiFi AP SSID
+#define WIFI_SSID "Replace with your WiFi Access Point identifier"
+// WiFi password
+#define WIFI_PASSWORD "Replace with your WiFi Password here"
+// InfluxDB v2 server url, e.g. https://eu-central-1-1.aws.cloud2.influxdata.com (Use: InfluxDB UI -> Load Data -> Client Libraries)
+#define INFLUXDB_URL "http://your.influx.db.server"
+// InfluxDB v2 server or cloud API authentication token (Use: InfluxDB UI -> Data -> Tokens -> <select token>)
+#define INFLUXDB_TOKEN "Replace with your Influx DB authentication token"
+// InfluxDB v2 organization id (Use: InfluxDB UI -> User -> About -> Common Ids )
+#define INFLUXDB_ORG "Replace with your InfluxDB Organization label"
+// InfluxDB v2 bucket name (Use: InfluxDB UI ->  Data -> Buckets)
+#define INFLUXDB_BUCKET "Replace with your InfluxDB Bucket label"
