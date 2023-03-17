a4957df18a18e895e311b079c98229f59121ae33
Regorlas <roger.random@outlook.com>
2023-03-17 12:49:06 -0700

    Minimal HTTP server with mDNS

--- pio_webserver/src/main.cpp
+++ pio_webserver/src/main.cpp
@@ -1,47 +1,48 @@
-#include <DNSServer.h>
 #ifdef ESP32
+#include <FS.h>
+#include <SPIFFS.h>
+#include <ESPmDNS.h>
 #include <WiFi.h>
 #include <AsyncTCP.h>
 #elif defined(ESP8266)
 #include <ESP8266WiFi.h>
 #include <ESPAsyncTCP.h>
+#include <ESP8266mDNS.h>
 #endif
-#include "ESPAsyncWebServer.h"
 
-DNSServer dnsServer;
+#include <ESPAsyncWebServer.h>
+
+#include <secrets.h>
+
+// SKETCH BEGIN
 AsyncWebServer server(80);
-
-class CaptiveRequestHandler : public AsyncWebHandler {
-public:
-  CaptiveRequestHandler() {}
-  virtual ~CaptiveRequestHandler() {}
-
-  bool canHandle(AsyncWebServerRequest *request){
-    //request->addInterestingHeader("ANY");
-    return true;
-  }
-
-  void handleRequest(AsyncWebServerRequest *request) {
-    AsyncResponseStream *response = request->beginResponseStream("text/html");
-    response->print("<!DOCTYPE html><html><head><title>Captive Portal</title></head><body>");
-    response->print("<p>This is out captive portal front page.</p>");
-    response->printf("<p>You were trying to reach: http://%s%s</p>", request->host().c_str(), request->url().c_str());
-    response->printf("<p>Try opening <a href='http://%s'>this link</a> instead</p>", WiFi.softAPIP().toString().c_str());
-    response->print("</body></html>");
-    request->send(response);
-  }
-};
-
+const char * hostName = "pio-webserver";
 
 void setup(){
-  //your other setup stuff...
-  WiFi.softAP("esp-captive");
-  dnsServer.start(53, "*", WiFi.softAPIP());
-  server.addHandler(new CaptiveRequestHandler()).setFilter(ON_AP_FILTER);//only when requested from AP
-  //more handlers...
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+  WiFi.mode(WIFI_AP_STA);
+  WiFi.begin(ssid, password);
+  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.printf("STA: Failed!\n");
+    WiFi.disconnect(false);
+    delay(1000);
+    WiFi.begin(ssid, password);
+  }
+
+  MDNS.begin(hostName);
+  MDNS.addService("http","tcp",80);
+
+  SPIFFS.begin();
+
+  server.serveStatic("/", SPIFFS, "/www/").setDefaultFile("index.html");
+
+  server.onNotFound([](AsyncWebServerRequest *request){
+    request->send(404);
+  });
   server.begin();
 }
 
 void loop(){
-  dnsServer.processNextRequest();
+  MDNS.update();
 }
